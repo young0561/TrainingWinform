598762fea3fddac7d1d640ba4e95f29ae9363cf3
young0561 <helo9809@gmail.com>
2020-06-15 12:30:54 +0900

    Generic Source update

--- GenericApp/GenericApp/Program.cs
+++ GenericApp/GenericApp/Program.cs
@@ -6,11 +6,53 @@ using System.Threading.Tasks;
 
 namespace GenericApp
 {
+    public class SimpleGeneric<T> {
+
+        private T[] values;
+        private int index;
+
+        public SimpleGeneric(int len) {
+
+            values = new T[len];
+            index = 0;
+        }
+
+        public void Add(params T[] args) {
+            foreach (T e in args)
+            {
+                values[index++] = e;
+            }
+        }
+
+        public void Print() {
+            foreach (T e in values)
+            {
+                Console.Write(e + "");
+            }
+                Console.WriteLine();
+            
+        }
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            SimpleGeneric<Int32> gIntegers = new SimpleGeneric<Int32>(10);
+            SimpleGeneric<double> gDouble = new SimpleGeneric<double>(10);
+
+
+            gIntegers.Add(1, 2);
+            gIntegers.Add(1, 2, 3, 4, 5, 6, 7);
+            gIntegers.Add(0);
+            gIntegers.Print();
+
+
+
+            gDouble.Add(10.0, 20.0, 30.0);
+            gDouble.Print();
+                 
         }
     }
 }
